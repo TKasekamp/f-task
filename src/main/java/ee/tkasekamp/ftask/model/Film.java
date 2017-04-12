8880c36ddd6e5c4a6b5e498ad66ba55a36fc2fa7
Tõnis Kasekamp <toniskasekamp@gmail.com>
2017-04-12 10:05:44 +0300

    Film class

--- src/main/java/ee/tkasekamp/ftask/model/Film.java
+++ src/main/java/ee/tkasekamp/ftask/model/Film.java
@@ -4,5 +4,11 @@ package ee.tkasekamp.ftask.model;
  * Created by Tõnis Kasekamp on 12.04.2017.
  */
 public class Film {
+    private String name;
+    private FilmType type;
 
+    public Film(String name, FilmType type) {
+        this.name = name;
+        this.type = type;
+    }
 }
