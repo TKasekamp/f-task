74086efa8b6163864792cff363a620f4b516121d
Tõnis Kasekamp <toniskasekamp@gmail.com>
2017-04-12 09:54:56 +0300

    Model classes

---
+++ src/main/java/ee/tkasekamp/ftask/model/Customer.java
@@ -0,0 +1,7 @@
+package ee.tkasekamp.ftask.model;
+
+/**
+ * Created by Tõnis Kasekamp on 12.04.2017.
+ */
+public class Customer {
+}
---
+++ src/main/java/ee/tkasekamp/ftask/model/Film.java
@@ -0,0 +1,8 @@
+package ee.tkasekamp.ftask.model;
+
+/**
+ * Created by Tõnis Kasekamp on 12.04.2017.
+ */
+public class Film {
+
+}
